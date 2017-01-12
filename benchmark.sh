5455d97c38576a2f45262199646a60767cf8e122
Michaelangel007 <testcraft@sharedcraft.com>
2017-01-12 13:48:44 -0800

    Cleanup alignment in benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -6,19 +6,19 @@
 
 cp data/words.txt words.txt
 
-echo -e "\nFile: test3.pl"; time perl src/test3.pl
-echo -e "\nFile: test3v1 "; time      bin/test3opt1
-echo -e "\nFile: test3v2 "; time      bin/test3opt2
-echo -e "\nFile: test3v3 "; time      bin/test3opt3
-echo -e "\nFile: test3v4 "; time      bin/test3opt4
-echo -e "\nFile: test3v5 "; time      bin/test3opt5
+ echo -e "\nFile: test3.pl"; time perl src/test3.pl
+ echo -e "\nFile: test3v1 "; time      bin/test3opt1
+ echo -e "\nFile: test3v2 "; time      bin/test3opt2
+ echo -e "\nFile: test3v3 "; time      bin/test3opt3
+ echo -e "\nFile: test3v4 "; time      bin/test3opt4
+ echo -e "\nFile: test3v5 "; time      bin/test3opt5
 
-echo -e "\n"; time      bin/test3opt6 -j1
-echo -e "\n"; time      bin/test3opt6 -j2
-echo -e "\n"; time      bin/test3opt6 -j3
-echo -e "\n"; time      bin/test3opt6 -j4
-echo -e "\n"; time      bin/test3opt6 -j5
-echo -e "\n"; time      bin/test3opt6 -j6
-echo -e "\n"; time      bin/test3opt6 -j7
-echo -e "\n"; time      bin/test3opt6 -j8
+ echo -e "\n";               time      bin/test3opt6 -j1
+ echo -e "\n";               time      bin/test3opt6 -j2
+ echo -e "\n";               time      bin/test3opt6 -j3
+ echo -e "\n";               time      bin/test3opt6 -j4
+ echo -e "\n";               time      bin/test3opt6 -j5
+ echo -e "\n";               time      bin/test3opt6 -j6
+ echo -e "\n";               time      bin/test3opt6 -j7
+ echo -e "\n";               time      bin/test3opt6 -j8
 
