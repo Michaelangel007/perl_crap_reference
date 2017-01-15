551d644854ff97ecd6b6acb0d87225e2608f5e64
Michaelangel007 <testcraft@sharedcraft.com>
2017-01-15 08:02:31 -0800

    More cleanup annotation

--- src/test3_annotated.c
+++ src/test3_annotated.c
@@ -3,8 +3,8 @@
                                         // *facepalm* Can't even compile due to missing: #include <string.h>
 
 char words[32000][260];                 // Magic numbers. Why 260? Should be cache line size: 256
-long wordidx = 0;                       // should be size_t, crappy _global_ var should have prefix: g_*;
-                                        // arg should be: char *word
+long wordidx = 0;                       // should be size_t; _global_ var should have prefix: g_*;
+                                        // arg should be: char *pWord
 void addtolist(char word[255])          // no whitespace makes this crap unreadable: add_to_list() OR AddToList()
 {
   int i=0;                              // useless re-init to zero
@@ -14,7 +14,7 @@ void addtolist(char word[255])          // no whitespace makes this crap unreada
           return;                       // inconsistent indentation -- why not 2 ???
     }
                                         // Off-by-one bug! Should be post-increment
-  strcpy(words[++wordidx],word);        // missing <string.h> similiar variables: words and word
+  strcpy(words[++wordidx],word);        // missing <string.h>; crappy similiar variables: `words` and `word` -- should be 'gLines` and `pWord`
 }
 
 int main()
